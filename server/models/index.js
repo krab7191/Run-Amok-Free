d94d3c48582436b58d7ce899d8541caa647a3082
Karsten Rabe <karstenrabe91@gmail.com>
2019-03-02 14:20:45 -0500

    Remove models we don't need

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,5 @@
 module.exports = {
     Users: require('./Users'),
-    Colors: require('./Colors'),
-    Drinks: require('./Drinks'),
-    Feedback: require('./Feedback'),
-    Keywords: require('./Keywords'),
-    Ratings: require('./Ratings'),
-    Settings: require('./Settings'),
-    Teams: require('./Teams')
+    Beverages: require('./Beverages'),
+    Notes: require('./Notes')
 };
