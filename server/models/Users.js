d4f8e54004155a5affab4ea9a9136abb7446bb38
Karsten Rabe <karstenrabe91@gmail.com>
2019-03-17 17:02:26 -0400

    Remove bcrypt in order to test on mobile

--- server/models/Users.js
+++ server/models/Users.js
@@ -5,7 +5,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema,
     // Password hashing: 1 increase in salt factor approximately doubles hashing time
-    bcrypt = require('bcrypt'),
     SALT_WORK_FACTOR = 10;
 
 const helpers = require('./mongooseHelperFunctions');
@@ -85,19 +84,6 @@ User.pre('save', function (next) {
     // only hash the password if it has been modified (or is new)
     if (!user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-        if (err) return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function (err, hash) {
-            if (err) return next(err);
-
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
-    });
 });
 
 module.exports = mongoose.model('Users', User);
--- server/passport/localStrategy.js
+++ server/passport/localStrategy.js
@@ -1,6 +1,5 @@
 const db = require('../models');
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require("bcrypt");
 
 const strategy = new LocalStrategy(
 	{
@@ -10,7 +9,6 @@ const strategy = new LocalStrategy(
 		db.Users.findOne({ 'email': username }, (err, user) => {
 			if (err) { return done(err); }
 			if (!user) { return done(null, false); }
-			if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
 			return done(null, user);
 		});
 	}
