28e20526f48d566b36d2a2b380a4eadd0a99bdce
Karsten Rabe <karstenrabe91@gmail.com>
2019-03-18 16:29:07 -0400

    Make toggles same green as save button

--- client/src/components/Switch/Switch.js
+++ client/src/components/Switch/Switch.js
@@ -1,82 +1,81 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Form from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Form from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
 const styles = theme => ({
-    iOSSwitchBase: {
-        '&$iOSChecked': {
-            color: theme.palette.common.white,
-            '& + $iOSBar': {
-                backgroundColor: '#52d869',
-            },
-        },
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-            easing: theme.transitions.easing.sharp,
-        }),
+  iOSSwitchBase: {
+    "&$iOSChecked": {
+      color: theme.palette.common.white,
+      "& + $iOSBar": {
+        backgroundColor: "#81C784"
+      }
     },
-    iOSChecked: {
-        transform: 'translateX(15px)',
-        '& + $iOSBar': {
-            opacity: 1,
-            border: 'none',
-        },
-    },
-    iOSBar: {
-        borderRadius: 13,
-        width: 42,
-        height: 26,
-        marginTop: -13,
-        marginLeft: -21,
-        border: 'solid 1px',
-        borderColor: theme.palette.grey[400],
-        backgroundColor: theme.palette.grey[50],
-        opacity: 1,
-        transition: theme.transitions.create(['background-color', 'border']),
-    },
-    iOSIcon: {
-        width: 24,
-        height: 24,
-    },
-    iOSIconChecked: {
-        boxShadow: theme.shadows[1],
-    },
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+      easing: theme.transitions.easing.sharp
+    })
+  },
+  iOSChecked: {
+    transform: "translateX(15px)",
+    "& + $iOSBar": {
+      opacity: 1,
+      border: "none"
+    }
+  },
+  iOSBar: {
+    borderRadius: 13,
+    width: 42,
+    height: 26,
+    marginTop: -13,
+    marginLeft: -21,
+    border: "solid 1px",
+    borderColor: theme.palette.grey[400],
+    backgroundColor: theme.palette.grey[50],
+    opacity: 1,
+    transition: theme.transitions.create(["background-color", "border"])
+  },
+  iOSIcon: {
+    width: 24,
+    height: 24
+  },
+  iOSIconChecked: {
+    boxShadow: theme.shadows[1]
+  }
 });
 
 class IosSwitch extends React.Component {
+  render() {
+    const { classes } = this.props;
+    const { _id } = this.props;
 
-    render() {
-        const { classes } = this.props;
-        const { _id } = this.props;
-
-        return (
-            <Form>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            classes={{
-                                switchBase: classes.iOSSwitchBase,
-                                bar: classes.iOSBar,
-                                icon: classes.iOSIcon,
-                                iconChecked: classes.iOSIconChecked,
-                                checked: classes.iOSChecked,
-                            }}
-                            disableRipple
-                            checked={this.props.isAvailable}
-                            onChange={e => this.props.handleToggle(e, _id)}
-                        />
-                    }
-                />
-            </Form>
-        );
-    }
+    return (
+      <Form>
+        <FormControlLabel
+          control={
+            <Switch
+              classes={{
+                switchBase: classes.iOSSwitchBase,
+                bar: classes.iOSBar,
+                icon: classes.iOSIcon,
+                iconChecked: classes.iOSIconChecked,
+                checked: classes.iOSChecked
+              }}
+              disableRipple
+              checked={this.props.isAvailable}
+              onChange={e => this.props.handleToggle(e, _id)}
+            />
+          }
+        />
+      </Form>
+    );
+  }
 }
 
 IosSwitch.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(IosSwitch);
