e7334d8a974b2eb13dad05d18d3856954d10c0d0
Karsten Rabe <karstenrabe91@gmail.com>
2019-03-25 19:01:33 -0400

    Resolves #64 - Non admin users only retrieve the notes they have submitted

--- client/src/pages/Notes/Notes.js
+++ client/src/pages/Notes/Notes.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { MyContext } from "../../components/MyContext/MyContext";
 import CommentCard from "../../components/CommentCard";
-import NoteSort from '../../components/NoteSort';
+import NoteSort from "../../components/NoteSort";
 
 import API from "../../utils/API";
 
@@ -11,86 +11,105 @@ const styles = {
   }
 };
 class Notes extends Component {
-  state = {
-    allNotes: [],
-    bevNames: [],
-    sort: "",
-    sortedNotes: []
-  };
-
-  componentWillMount () {
-    this.getNoteData(this.getBevNames);
+  constructor(props) {
+    super(props);
+    this.state = {
+      allNotes: [],
+      bevNames: [],
+      sort: "",
+      sortedNotes: [],
+      fetched: false
+    };
   }
 
   filterBevNames = () => {
     let arr = [];
-    this.state.allNotes.forEach(note => {
-      if (arr.indexOf(note.beverages.name) === -1) {
-        arr.push(note.beverages.name);  
-      }
-    });
+    this.state.allNotes.length > 0 &&
+      this.state.allNotes.forEach(note => {
+        if (arr.indexOf(note.beverages.name) === -1) {
+          arr.push(note.beverages.name);
+        }
+      });
     return arr;
-  }
+  };
 
   getBevNames = () => {
     const bevArr = this.filterBevNames();
     console.log(bevArr);
-    this.setState({bevNames: bevArr});
-  }
+    this.setState({ bevNames: bevArr });
+  };
 
-  handleNotesSort = (sel) => {
+  handleNotesSort = sel => {
     if (sel !== "All") {
-      let sortArr = this.state.allNotes.filter(note => note.beverages.name === sel);
+      let sortArr = this.state.allNotes.filter(
+        note => note.beverages.name === sel
+      );
       this.setState({
         sortedNotes: sortArr
-      })
-    }
-    else {
+      });
+    } else {
       this.setState({
         sortedNotes: this.state.allNotes
-      })
+      });
     }
-  }
+  };
 
-  getNoteData = (cb) => {
-    API.getNoteData()
-      .then((res) => {
+  getNoteData = (cb, userid) => {
+    API.getNoteData(userid)
+      .then(res => {
         this.setState(
           {
             allNotes: res.data,
-            sortedNotes: res.data,        
+            sortedNotes: res.data,
+            fetched: true
           },
-          () => 
-          {
-            console.log("state ",this.state);
+          () => {
+            console.log("state ", this.state);
             cb();
           }
         );
       })
       .catch(err => console.log(err));
-  }
+  };
 
   render() {
-    return(
+    return (
       <MyContext.Consumer>
         {context => {
-            const sortedNotes = this.state.sortedNotes;
-            return(
-              <div className="main">
-                <h1 style={styles.header}>Notes</h1>
-                <NoteSort bevNames={this.state.bevNames} sort={this.handleNotesSort} />
-                {this.state.sortedNotes.map((comment,index)=> (
-                  <CommentCard 
-                    key={index} 
-                    leftBy={comment.user ? comment.user.firstName : ""} 
-                    name={comment.beverages.name} 
-                    comment={comment.body} />
-                ))}
-                {sortedNotes.length === 0 && (
-                <p className="text-center">No one has left any notes yet! Time to have a tasting.</p>
+          const sortedNotes = this.state.sortedNotes;
+          const { _id } = context.myState.user;
+
+          this.state.allNotes.length === 0 &&
+            this.state.fetched === false &&
+            this.getNoteData(this.getBevNames, _id);
+
+          return (
+            <div className="main">
+              <h1 style={styles.header}>Notes</h1>
+              {this.state.bevNames && (
+                <NoteSort
+                  bevNames={this.state.bevNames}
+                  sort={this.handleNotesSort}
+                />
               )}
-              </div>
-        )}}
+
+              {this.state.sortedNotes.length > 0 &&
+                this.state.sortedNotes.map((comment, index) => (
+                  <CommentCard
+                    key={index}
+                    leftBy={comment.user ? comment.user.firstName : ""}
+                    name={comment.beverages.name}
+                    comment={comment.body}
+                  />
+                ))}
+              {sortedNotes.length === 0 && (
+                <p className="text-center">
+                  No notes yet! Time to have a tasting.
+                </p>
+              )}
+            </div>
+          );
+        }}
       </MyContext.Consumer>
     );
   }
--- server/routes/api/get.js
+++ server/routes/api/get.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const getController = require('../../controllers/getController');
 
 // Private GET routes here:
-// router.get('/secureData', secureGetController.secureData);
-router.route('/notes')
-    .get(getController.getAllNotes);
+router.get('/notes/:_id', getController.getAllNotes);
 
 router.route('/allBeverages')
     .get(getController.getAllBeverages);
