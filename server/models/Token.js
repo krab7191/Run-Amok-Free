415eb3beeb9c43151847c0ae1bcf89b0c6e1c6b6
Jon Jackson <ocskier@gmail.com>
2019-03-27 16:45:46 -0400

    made Token Model

---
+++ server/models/Token.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const helpers = require("./mongooseHelperFunctions");
+
+const Token = new Schema({
+    token: {
+        type: String,
+        required: "A token is required for registration!"
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        validate: [helpers.email, "Please enter a valid email address."]
+    },
+    deleteOnRead: {
+        type: Boolean,
+        default: false
+    },
+    validUntil: {
+        type: Date,
+        default: Date().now + 30*60*1000
+    }
+
+});
+
+module.exports = mongoose.model('Tokens',Token);
