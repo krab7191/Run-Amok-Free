1122a35d60cdc464325d35bfa8c89332b17a1d7f
Karsten Rabe <karstenrabe91@gmail.com>
2019-03-30 17:03:54 -0400

    Change token expiry period to 5 days

--- server/models/Tokens.js
+++ server/models/Tokens.js
@@ -4,24 +4,28 @@ const Schema = mongoose.Schema;
 const helpers = require("./mongooseHelperFunctions");
 
 const Token = new Schema({
-    token: {
-        type: String,
-        required: "A token is required for registration!"
-    },
-    email: {
-        type: String,
-        trim: true,
-        validate: [helpers.email, "Please enter a valid email address."]
-    },
-    deleteOnRead: {
-        type: Boolean,
-        default: false
-    },
-    validUntil: {
-        type: Date,
-        default: +new Date() + 30*60*1000
-    }
-
+  token: {
+    type: String,
+    required: "A token is required for registration!"
+  },
+  email: {
+    type: String,
+    trim: true,
+    validate: [helpers.email, "Please enter a valid email address."]
+  },
+  deleteOnRead: {
+    type: Boolean,
+    default: false
+  },
+  validUntil: Date
 });
 
-module.exports = mongoose.model('Tokens',Token);
+Token.pre("save", function(next) {
+  let token = this;
+  const d = new Date();
+  d.setDate(d.getDate() + 5);
+  token.validUntil = d;
+  next();
+});
+
+module.exports = mongoose.model("Tokens", Token);
